ae7f1aa3f3eee6ae0d5bfcae86656422dde67ddc
Victor Hugo Viana <torugosis@gmail.com>
2021-08-16 22:33:20 -0300

    refactor: refatora chamadas do wordometers
    Otimiza chamadas da URL, para cada métrica era chamada novamente o site onde se colhe as métricas, esse commit otimiza o código em 4x

--- app/web_app/main.py
+++ app/web_app/main.py
@@ -35,40 +35,43 @@ class PageVisit:
         return self.browser.page_source
 
 
-def get_metric_from_worldometers(rel):
-    html = PageVisit().get_html("https://www.worldometers.info")
+class MetricsWorldometers:
+    def __init__(self):
+        html = PageVisit().get_html("https://www.worldometers.info")
+        self.bs = BeautifulSoup(html, "html.parser")
 
-    bs = BeautifulSoup(html, "html.parser")
+    def get_by_rel(self, rel):
+        parent = self.bs.find("span", {"rel": rel})
+        numbers = parent.findChildren("span")
 
-    parent = bs.find("span", {"rel": rel})
-    numbers = parent.findChildren("span")
+        # Provavelmente não é o melhor jeito de pegar esse valor numérico mas funciona
+        # TODO: melhorar essa metodologia de criação do número
+        metric_str = ""
+        for number in numbers:
+            try:
+                if int(number.text) > 0:
+                    metric_str += number.text
+            except:
+                pass
 
-    # Provavelmente não é o melhor jeito de pegar esse valor numérico mas funciona
-    # TODO: melhorar essa metodologia de criação do número
-    metric_str = ""
-    for number in numbers:
-        try:
-            if int(number.text) > 0:
-                metric_str += number.text
-        except:
-            pass
-
-    return int(metric_str)
+        return int(metric_str)
 
 
 def metrics_from_worldometers():
+    metrics_worldometers = MetricsWorldometers()
+
     return {
-        "perda_de_floresta_este_ano": get_metric_from_worldometers(
+        "perda_de_floresta_este_ano": metrics_worldometers.get_by_rel(
             "forest_loss/this_year"
         ),
-        "emissoes_co2_este_ano": get_metric_from_worldometers(
+        "emissoes_co2_este_ano": metrics_worldometers.get_by_rel(
             "co2_emissions/this_year"
         ),
-        "barris_de_petroleo_restante": get_metric_from_worldometers("oil_reserves"),
-        "populacao_sem_acesso_a_agua_potavel": get_metric_from_worldometers(
+        "barris_de_petroleo_restante": metrics_worldometers.get_by_rel("oil_reserves"),
+        "populacao_sem_acesso_a_agua_potavel": metrics_worldometers.get_by_rel(
             "nowater_population"
         ),
-        "desertificacao_este_ano": get_metric_from_worldometers(
+        "desertificacao_este_ano": metrics_worldometers.get_by_rel(
             "desert_land_formed/this_year"
         ),
     }
